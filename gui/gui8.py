84914c20ad36907372c7176f486710c47bc890e7
jritsqmet <jroserog@itsqmet.edu.ec>
2025-03-20 10:18:30 -0500

    gui 8

--- gui/gui8.py
+++ gui/gui8.py
@@ -1,5 +1,6 @@
 # Formulario que pida al usuario, su nombre, la edad
-# el género, estado civil, número de cédula
+# el género, estado civil, número de cédula, 
+# AGREGAR UN CHECKBOX, CON LENGUAJES DE PROGRAMACIÓN
 # En base a la información  se crearán 2 botones
 # 1. (Ver información) Mostrar la informacacion ingresada
 # 2. (Calculo de impuestos)
@@ -9,7 +10,52 @@
 #  Además si en el esta civil consta como soltero se agrega un impuesto $30
 
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
+
+def verInformacion():
+    cedula = entrada1.get()
+    nombre = entrada2.get()
+    edad =  entrada3.get()
+    genrer = genero.get()
+    civil = entrada4.get()
+
+    lenguajes = []
+
+    if python.get()== True:
+        lenguajes.append("Python")
+    
+    if cpp.get() == True:
+        lenguajes.append("C++")
+
+    if go.get() == True :
+        lenguajes.append("Go")
+
+
+
+    messagebox.showinfo("Datos Ingresados",
+                       "Cédula: "+ cedula+ "\nNombre: " + nombre + "\nedad: "+ str(edad)   +"\nGénero: " + genrer+ "\nEstado Civil: " + civil
+                        +f"\nLenguajes:  { str(lenguajes ) }"
+                         )
+
+
+def calcularImpuesto():
+    nombre = entrada2.get()
+    edad = int ( entrada3.get() )
+    civil = entrada4.get()
+    impuesto=0
+
+    if edad>=20 and edad < 60:
+        impuesto = 200
+    elif edad >= 60:
+        impuesto = 100
+
+    if civil == "Soltera/o":
+        impuesto = impuesto + 30
+
+    messagebox.showinfo("Valor impuesto", f"{nombre} debe pagar ${impuesto} por impuestos" )
+
+
+
 
 ventana = tk.Tk()
 ventana.title("CALCULADORA IMPUESTOS")
@@ -33,8 +79,8 @@ entrada3.grid(row=3, column=1)
 ### GÉNERO ###
 tk.Label(ventana, text="Género: ").grid(row=4, column=0)
 genero = tk.StringVar()
-rb_femenino  = tk.Radiobutton(ventana, text="Femenino", value="F", variable= genero)
-rb_masculino = tk.Radiobutton(ventana, text="Masculino", value="M", variable= genero)
+rb_femenino  = tk.Radiobutton(ventana, text="Femenino", value="Femenino", variable= genero)
+rb_masculino = tk.Radiobutton(ventana, text="Masculino", value="Masculino", variable= genero)
 
 rb_femenino.grid( row =4 , column=1)
 rb_masculino.grid( row =4 , column=2)
@@ -42,9 +88,26 @@ rb_masculino.grid( row =4 , column=2)
 ### ESTADO CIVIL ###
 tk.Label(ventana, text="Estado Civil: ").grid(row=5, column=0)
 estado= ["Soltera/o", "Casada/o", "Divorciada/o", "Viuda/o"]
-entrada3 = ttk.Combobox(ventana, values= estado)
-entrada3.grid(row=5, column=1)
+entrada4 = ttk.Combobox(ventana, values= estado, state='readonly')
+entrada4.grid(row=5, column=1)
 
-tk.Button(ventana, text="Ver información", command="").grid(row=6, column=0)
-tk.Button(ventana, text="Calcula impuestos", command="").grid(row=6, column=2)
+### LENGUAJES DE PROGRAMACIÓN ####
+python = tk.BooleanVar()
+cpp = tk.BooleanVar()
+go= tk.BooleanVar()
+
+tk.Label(ventana, text="Seleccionar lenguajes: ").grid(row=6, column=0)
+cb_python = tk.Checkbutton(ventana, text="Python", variable=python)
+cb_python.grid(row=6, column=1)
+
+cb_cpp = tk.Checkbutton(ventana, text="C++", variable=cpp)
+cb_cpp.grid(row=7, column=1)
+
+cb_go = tk.Checkbutton(ventana, text="Go", variable= go)
+cb_go.grid(row=8, column=1)
+
+tk.Button(ventana, text="Ver información", command=verInformacion).grid(row=9, column=0)
+tk.Button(ventana, text="Calcula impuestos", command=calcularImpuesto).grid(row=9, column=2)
+
+
 ventana.mainloop()
